96ce3e22fdd02e2915bfcbe2d0e2dfa75a2c8c7d
mrumpf <michael@rumpfonline.de>
2017-12-12 11:32:32 -0700

    Finished the multipole JAR lab

--- components/movies/src/main/java/org/superbiz/moviefun/ActionServlet.java
+++ components/movies/src/main/java/org/superbiz/moviefun/ActionServlet.java
@@ -17,11 +17,11 @@
 package org.superbiz.moviefun;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.superbiz.moviefun.movies.Movie;
 import org.superbiz.moviefun.movies.MoviesBean;
 
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -39,8 +39,11 @@ public class ActionServlet extends HttpServlet {
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
     private MoviesBean moviesBean;
+
+    public ActionServlet(@Autowired MoviesBean moviesBean) {
+        this.moviesBean = moviesBean;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
